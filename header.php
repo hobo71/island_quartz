ec08dde871966b0c2d026dd2da43651cfbaa6c7d
Ben Kaminski <ben@benkaminski.com>
2018-09-15 19:10:28 -0400

    more required functionality added to WP theme

--- header.php
+++ header.php
@@ -1,10 +1,9 @@
 <!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Hello, world!</title>
-    <?php wp_head(); ?>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<h1>Hello, world!</h1>
