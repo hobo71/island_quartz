401fdafb34541313f762d2d0d2499ca22e0fb8b7
Ben Kaminski <ben@benkaminski.com>
2018-09-24 17:20:14 -0400

    added header image and main menu

--- front-page.php
+++ front-page.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
-
-
-
+<header class="liq-header">
+<div class="container-fluid">
+</div>
+</header>
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -6,4 +6,20 @@
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<h1>Hello, world!</h1>
+<nav class="navbar navbar-expand-md fixed-top">
+<a class="navbar-brand text-white" href="<?php echo get_home_url(); ?>"><span class="navbar-brand mb-0 h1"><?php echo get_bloginfo('name'); ?></span></a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"><i class="fas fa-bars fa-fw fa-2x text-white"></i></button>
+<div class="collapse navbar-collapse" id="navbarCollapse">
+<?php wp_nav_menu( array(
+'theme_location'    => 'top-navigation',
+'depth'             => 2,
+'container'         => '',
+'container_class'   => '',
+'container_id'      => '',
+'menu_class'        => 'navbar-nav',
+'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+'walker'            => new WP_Bootstrap_Navwalker(),
+)); ?>
+</div>
+</nav>
+<div class="clearfix"></div>
