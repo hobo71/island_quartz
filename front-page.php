4f3d40dae9294b3380f8d21fbc852c110a237372
Ben Kaminski <ben@benkaminski.com>
2018-09-25 11:02:29 -0400

    added header elements

--- front-page.php
+++ front-page.php
@@ -1,6 +1,16 @@
 <?php get_header(); ?>
 <header class="liq-header">
 <div class="container-fluid">
+	<div class="container">
+		<div class="row pt-5">
+			<div class="col-md-5 pt-5">
+				<img id="diamond" src="<?php echo get_template_directory_uri(); ?>/lib/img/long-island-quartz-diamond.png" class="island-diamond" alt="Long Island Quartz" />
+			</div>
+			<div class="col-md-7">
+				<h1 class="text-white display-1 title-text">Long Island<br />Quartz</h1>
+			</div>
+		</div>
+	</div>
 </div>
 </header>
 <?php get_footer(); ?>
