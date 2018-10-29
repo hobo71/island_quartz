b91ba3b19b36e4558a09f41a3373fa794c2f0d6e
Ben Kaminski <ben@benkaminski.com>
2018-10-29 19:30:09 -0400

    started editing dom via jquery, added boilerplate content for pages.

---
+++ tag.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<div class="container interiorPages">
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<h1 class="pt-3 display-4"><?php the_title(); ?></h1>
+
+		<?php the_content(); ?>
+
+
+
+
+	<?php endwhile; else : ?>
+		<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+	<?php endif; ?>
+</div>
+<?php get_footer(); ?>
